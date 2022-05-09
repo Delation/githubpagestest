2626d49c2a4485cc50221388166bdd8735462a56
Yeti <88898324+Delation@users.noreply.github.com>
2022-05-09 11:24:48 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
 from browser import document
 
+if document.cookie:
+  document['user_out'].textContent = 'Welcome back, %s' % document.cookie[5:]
+
+def save_user(e):
+  if not e.key == 'Enter':
+    return
+  document.cookie = "user=%s" % e.target.value
+  document['user_out'].textContent = 'Saved your username, %s' % e.target.value
+
 def show_text(e):
   if not e.key == 'Enter':
     return
@@ -12,3 +21,6 @@ def show_text(e):
   #document['output'].textContent = e.target.value;
 
 document['text'].bind('keypress', show_text)
+
+
+document['user_in'].bind('keypress', show_text)
